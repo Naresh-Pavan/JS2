2f5f26c7c630a0c940f3c828a988bc8540ee5e8f
Naresh-Pavan <71119630+Naresh-Pavan@users.noreply.github.com>
2022-03-18 20:41:51 +0530

    completed

---
+++ script.js
@@ -0,0 +1,9 @@
+var str="Skill Safari";
+console.log("Length of the string is",str.length);
+console.log("Capital letters-",str.toUpperCase())
+console.log("Index of 'a' is",str.indexOf("a"));
+console.log("Steve Rogers : Big man in a suit of armour. Take that off, what are you?\nTony Stark : Genius, billionaire, playboy, philanthropist.\nSteve Rogers : I know guys with none of that worth ten of you. I've seen the footage. The only thing you really fight for is yourself. You're not the guy to make the sacrifice play, to lay down on a wire and let the other guy crawl over you.\nTony Stark : I think I would just cut the wire.\nSteve Rogers : Always a way out... You know, you may not be a threat, but you better stop pretending to be a hero.\nTony Stark : A hero? Like you? You're a lab rat, Rogers. Everything special about you came out of a bottle!\nSteve Rogers : Put on the suit. Let's go a few rounds.");
+let height=1.8;
+let weight=60;
+let BMI=weight/height;
+console.log("BMI of a person with height 1.8mtr and weight 60kg is",BMI)
